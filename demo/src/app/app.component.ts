785064c2ac9baa75dab0430aeaa1f37d87eed174
cipchk <cipchk@qq.com>
2017-11-02 18:42:16 +0800

    build: Support Angular5, releases: 1.0.1

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
     selector: 'app-root',
     template: `
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" routerLink="/">ngx-ueditor</a>
+        <a class="navbar-brand" routerLink="/">angular-baidu-maps</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
