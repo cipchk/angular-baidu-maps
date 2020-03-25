d5353bed0d0988dab635ba9409f805637be2ec3c
mo-gong <50678280@qq.com>
2020-03-25 14:59:39 +0800

    feat: angular9

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,1 @@
-export { AbmConfig } from './src/abm.config';
-export { AbmComponent } from './src/abm.component';
-export { AbmPanoramaComponent } from './src/abm-panorama.component';
-export * from './src/abm.module';
+export * from './public_api';
---
+++ lib/public_api.ts
@@ -0,0 +1,4 @@
+export { AbmConfig } from './src/abm.config';
+export { AbmComponent } from './src/abm.component';
+export { AbmPanoramaComponent } from './src/abm-panorama.component';
+export * from './src/abm.module';
